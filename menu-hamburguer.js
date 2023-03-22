374f41045d7e7b2add859ea2df845ba8cbcb05af
Mah <madavetto@gmail.com>
2023-03-22 00:05:43 -0300

    update

--- menu-hamburguer.js
+++ menu-hamburguer.js
@@ -1,10 +1,15 @@
-const nav__button__mobiles = document.getElementsByClassName('nav__button__mobile');
+/*const nav__button__mobiles = document.getElementsByClassName('nav__button__mobile');
 
-function toggleMenu() {
+function toggleMenu(event) {
+    if(event.type === 'touchstart') event.preventDefault()
     const nav__bars = document.getElementsByClassName('nav__bar');
     nav__bars[0].classList.toggle('active');
 }
 
 nav__button__mobiles[0].addEventListener('click', toggleMenu);
+nav__button__mobiles[0].addEventListener('touchstart', toggleMenu);
+
+
+*/
 
 
