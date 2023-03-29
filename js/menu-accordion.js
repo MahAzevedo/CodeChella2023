b4ef5d732d7b840498c7306ef1c04008f2a00a65
Mah <madavetto@gmail.com>
2023-03-29 15:24:47 -0300

    update

--- js/menu-accordion.js
+++ js/menu-accordion.js
@@ -10,59 +10,34 @@
 }*/
 
 
+/* >>>> FUNCIONOU <<<< 
 function clickAccordion(t) {
     document.getElementsByClassName('titulo')
     [t].parentElement.style.backgroundColor = "#2E7BA2";
 }
+*/
+
+/*  >>>>>  SUPER FUNCIONOU :D <<<<<
+function clickAccordion(t) {
+    var pai = document.getElementsByClassName('titulo')[t].parentElement;
+    var conteudo = pai.lastElementChild.classList.contains('hide');
+
+    console.log(conteudo);
+}
+*/
 
 
+function clickAccordion(t) {
+    var pai = document.getElementsByClassName("titulo")[t].parentElement;
+    var conteudo = pai.lastElementChild.classList.contains("hide");
 
-
-
-/*function clickAccordion('hide') {
-    document.getElementsByClassName("accordion-titulo").item = "hide";
-}*
-
-/*
-const menu = document.querySelector(".nav__lista__ul");
-
-botao.addEventListener("click", () =>) {
-    if (menu.style.display == "none") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none"
+    if(conteudo == true) {
+        pai.lastElementChild.className = "show";
+    }else {
+        pai.lastElementChild.className = "hide";
     }
-}
-*/
 
-/*const nav__button__mobiles = document.getElementsByClassName('nav__button__mobile');
-
-function toggleMenu(event) {
-    if(event.type === 'touchstart') event.preventDefault()
-    const nav__bars = document.getElementsByClassName('nav__bar');
-    nav__bars[0].classList.toggle('active');
+    console.log(conteudo);
 }
 
-nav__button__mobiles[0].addEventListener('click', toggleMenu);
-nav__button__mobiles[0].addEventListener('touchstart', toggleMenu);
 
-
-*/
-
-/* esse hamburguer é o mais simples que funciona bem 
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
-
-*/
- 
-
-/*
-const hamburguer = document.querySelector(".hamburguer");
-const nav__menu = document.querySelector(".nav__menu");
-
-hamburguer.addEventListener("click", () -> ) {
-  hamburguer.classList.toggle("active");
-  nav__menu.classList.toggle("active");
-}
-*/
