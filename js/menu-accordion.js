d3c17d0658fd1dd8e400d693b9df0706a9e34e95
Mah <madavetto@gmail.com>
2023-03-29 22:33:45 -0300

    update

--- js/menu-accordion.js
+++ js/menu-accordion.js
@@ -50,7 +50,8 @@ function clickAccordion(t) {
 }*/
 
 
-// FUNCIONOU
+// FUNCIONOU 5exemplo, que leva ao 6exemplo
+/*
 function clickAccordion(t) {
     let pai = document.getElementsByClassName("accordion-drop")[t].parentElement;
     let img = document.getElementsByClassName("seta")[t];
@@ -64,5 +65,42 @@ function clickAccordion(t) {
         img.style.transform = "rotate("+0+"deg)";
     }
 
-    console.log(conteudo);
+    //console.log(conteudo);
+}
+*/
+
+// 6exemplo, com o dropdown descendo e subindo, que leva ao 7exemplo:
+// tirei o .lastElementChild pq nao sera usado mais, agora sera: sectionHide
+/*function clickAccordion(t) {
+    let pai = document.getElementsByClassName("accordion-drop")[t].parentElement;
+    let img = document.getElementsByClassName("seta")[t];
+    let conteudo = pai.classList.contains("sectionHide");
+    //console.log(document.getElementsByClassName('section'));
+
+    if(conteudo == true) {
+        pai.className = "section sectionShow";
+        img.style.transform = "rotate("+-90+"deg)";
+    }else {
+        pai.className = "section sectionHide";
+        img.style.transform = "rotate("+0+"deg)";
+    }
+
+}*/
+
+
+// 7exemplo
+function clickAccordion(t) {
+    let pai = document.getElementsByClassName("accordion-drop")[t].parentElement;
+    let img = document.getElementsByClassName("seta")[t];
+    let conteudo = pai.classList.contains("sectionHide");
+    let sections = document.getElementsByClassName("section");
+
+    if(conteudo == true) {
+        pai.className = "section sectionShow";
+        img.style.transform = "rotate("+-90+"deg)";
+    }else {
+        pai.className = "section sectionHide";
+        img.style.transform = "rotate("+0+"deg)";
+    }
+
 }
